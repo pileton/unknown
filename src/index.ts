e6a7c23324b3fddad5abf6f9dc0e87dedb17d16d
pileton <pileton73@gmail.com>
2026-04-09 22:29:59 +0200

    Implement onLoad and onUnload methods for module

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
+import { logger } from "@vendetta";
+import { applyPatches } from "./patches";
+import Settings from "./settings";
+import { startFluxInterceptor } from "./flux";
+import "./storage";
 
+let patchUnloads: Array<() => void> = [];
+let stopFluxInterceptor: (() => void) | undefined;
+
+export default {
+    onLoad() {
+        stopFluxInterceptor?.();
+        patchUnloads.forEach((unpatch) => unpatch());
+
+        patchUnloads = applyPatches();
+        stopFluxInterceptor = startFluxInterceptor();
+
+        logger.log("OmniArchive loaded.");
+    },
+    onUnload() {
+        stopFluxInterceptor?.();
+        stopFluxInterceptor = undefined;
+
+        patchUnloads.splice(0).reverse().forEach((unpatch) => unpatch());
+
+        logger.log("OmniArchive unloaded.");
+    },
+    settings: Settings,
+};
